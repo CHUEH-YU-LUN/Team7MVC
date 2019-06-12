ba18a84348f1640c3ebf7896a58f164a2245d06d
YU-LING CHEN <wsx789888888@gmail.com>
2019-06-13 07:09:28 +0800

    modify Customer/PurchaseList

--- Team7MVC/Controllers/CustomerController.cs
+++ Team7MVC/Controllers/CustomerController.cs
@@ -80,5 +80,12 @@ namespace Team7MVC.Controllers
             var orders = _repo.OrderQuery(User.Identity.Name);
             return View(orders);
         }
+        [HttpGet]
+        public ActionResult Order_Details(int Id)
+        {
+            var orders_Details = _repo.QueryOrderDetails(Id);
+            return View(orders_Details);
+        }
+
     }
 }
